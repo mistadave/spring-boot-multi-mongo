041531d6d2571795f2995604599a0db4b8b08d27
mistadave <std@lemt.ch>
2020-02-19 12:56:22 +0100

    add data rest, mongo data

--- src/main/java/com/marcosbarbero/wd/multiplemongo/repository/primary/PrimaryRepository.java
+++ src/main/java/com/marcosbarbero/wd/multiplemongo/repository/primary/PrimaryRepository.java
@@ -1,9 +1,11 @@
 package com.marcosbarbero.wd.multiplemongo.repository.primary;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 /**
  * @author Marcos Barbero
  */
+@RepositoryRestResource(collectionResourceRel = "first", itemResourceRel = "first", path = "first")
 public interface PrimaryRepository extends MongoRepository<PrimaryModel, String> {
 }
--- src/main/java/com/marcosbarbero/wd/multiplemongo/repository/secondary/SecondaryRepository.java
+++ src/main/java/com/marcosbarbero/wd/multiplemongo/repository/secondary/SecondaryRepository.java
@@ -1,9 +1,11 @@
 package com.marcosbarbero.wd.multiplemongo.repository.secondary;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 /**
  * @author Marcos Barbero
  */
+@RepositoryRestResource(collectionResourceRel = "second", itemResourceRel = "second", path = "second")
 public interface SecondaryRepository extends MongoRepository<SecondaryModel, String> {
 }
